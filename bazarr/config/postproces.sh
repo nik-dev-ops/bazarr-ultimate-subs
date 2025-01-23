29f88267b8fd7e4d727a3cf1c8904bfa72820445
nik-dev-ops <ninokotur@gmail.com>
2025-01-23 16:50:15 +0100

    updated postprocessing script to run always except if embeded subtitles

--- bazarr/config/postproces.sh
+++ bazarr/config/postproces.sh
@@ -4,14 +4,14 @@ episode="$1"
 subtitles="$2"
 provider="$3"
 
-# Check if the subtitle was fetched from the specific provider (e.g., "whisperai")
-if [[ "$provider" == "whisperai" ]]; then
-    echo "Post-processing subtitles from whisperai"
+# Check if the provider is NOT "embeddedsubtitles"
+if [[ "$provider" != "embeddedsubtitles" ]]; then
+    echo "Post-processing subtitles (provider is not embeddedsubtitles)"
 
     curl -s -X POST http://aeneas:5000/process \
     -H "Content-Type: application/json" \
     -d "{\"video_file_path\":\"$episode\",\"subtitle_file_path\":\"$subtitles\"}"
 
 else
-    echo "Skipping post-processing. Subtitle not fetched from whisperai"
+    echo "Skipping post-processing. Subtitle provider is embeddedsubtitles"
 fi
